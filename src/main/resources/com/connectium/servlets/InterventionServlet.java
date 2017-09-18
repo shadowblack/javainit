10e0c091e1bb1ebfd72874c016f32e23630bc84f
Luis Marin <ninja.aoshi@gmail.com>
2017-09-18 17:55:47 -0400

    Se sube diagrama UML

--- src/main/resources/com/connectium/servlets/InterventionServlet.java
+++ src/main/resources/com/connectium/servlets/InterventionServlet.java
@@ -1,8 +1,16 @@
 package com.connectium.servlets;
 
-import com.connectium.controllers.Email;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageOutputStream;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +20,7 @@ import javax.servlet.http.Part;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * Created by lmarin on 12/6/2017.
@@ -19,19 +28,54 @@ import java.nio.file.Paths;
 @WebServlet(urlPatterns = "/upload",name = "Inter")
 public class InterventionServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("image/jpeg");
 
-        Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+        try {
+            List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+            for (FileItem item : items) {
+                if (item.isFormField()) {
+                    // Process regular form field (input type="text|radio|checkbox|etc", select, etc).
+                    String fieldname = item.getFieldName();
+                    String fieldvalue = item.getString();
+                    // ... (do your job here)
+                } else {
+                    // Process form file field (input type="file").
+                    String fieldname = item.getFieldName();
+                    String filename = FilenameUtils.getName(item.getName());
+                    InputStream filecontent = item.getInputStream();
+                    // ... (do your job here)
 
-        String pathToWeb = getServletContext().getRealPath(File.separator);
-        //File f = new File(pathToWeb + "avajavalogo.jpg");
-       // String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
-        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
-        InputStream fileContent = filePart.getInputStream();
-        BufferedImage bi = ImageIO.read(fileContent);
-        OutputStream out = response.getOutputStream();
-        ImageIO.write(bi, "jpg", out);
-        out.close();
+
+                    response.setContentType("image/jpeg");
+
+                   // Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+
+                    String pathToWeb = getServletContext().getRealPath(File.separator);
+                    //File f = new File(pathToWeb + "avajavalogo.jpg");
+                    // String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+                    //String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+                    InputStream fileContent = filecontent/*filePart.getInputStream()*/;
+                    BufferedImage bi = ImageIO.read(fileContent);
+
+                    ImageWriter writer = (ImageWriter)ImageIO.getImageWritersByFormatName("jpeg").next();
+                    ImageWriteParam iwp = writer.getDefaultWriteParam();
+                    iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+                    iwp.setCompressionQuality(1);
+
+                    //FileImageOutputStream output = new FileImageOutputStream(file);
+
+                    writer.setOutput(bi);
+
+                    OutputStream out = response.getOutputStream();
+                    ImageIO.write(bi, "jpeg", out);
+                    out.close();
+
+                }
+            }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        }
+
+
 
       /*  String description = request.getParameter("description"); // Retrieves <input type="text" name="description">
         Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
